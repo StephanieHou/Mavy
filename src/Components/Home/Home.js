3e3d3b17473875bd6d98b7873d5a44201be93711
Stephanie Hou <stephaniehou@Stephanies-Air.lan>
2021-04-16 16:17:49 -0400

    Firebase Hosting Set Up : https://mavy-b0112.web.app/

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+
+  return (
+    <div className='Home'>
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+import "firebase/auth";
+
+const fireApp = firebase.initializeApp({
+    apiKey: "AIzaSyC9qoAA1Wj4shWPWqA5uX2ERKcJAORv3JM",
+    authDomain: "mavy-b0112.firebaseapp.com",
+    projectId: "mavy-b0112",
+    storageBucket: "mavy-b0112.appspot.com",
+    messagingSenderId: "1059536561956",
+    appId: "1:1059536561956:web:8be0d1cd4b63e23c7835b1",
+    measurementId: "G-PWZ7NKGS2K"
+});
+
+
+export default fireApp;
