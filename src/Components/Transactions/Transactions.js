28d6096c996e3db86b32226016015abc366d663e
Michael Fuery <mfuery@gmail.com>
2021-04-20 14:36:04 -0700

    Transaction not working...

--- src/Components/Transactions/Transactions.js
+++ src/Components/Transactions/Transactions.js
@@ -1,45 +1,29 @@
-import app from "firebase/app";
-import "firebase/database";
-import { Main, Navbar, Section } from "../Styled";
-import { useState } from "react";
+import { Main, Navbar } from "../Styled";
 
-// const GET_STEPZEN = gql`
-//   query Transactions($username: String = "michael") {
-//     transactions(username: $username) {
-//       amount
-//       category
-//       company
-//       date
-//       username
-//     }
-//   }
-// `;
+const GET_STEPZEN = gql`
+  query Transactions($username: String = "michael") {
+    transactions(username: $username) {
+      username
+      amount
+      category
+      company
+      purchaseDate
+      createdAt
+    }
+  }
+`;
 
 const Transactions = () => {
-  const username = "michael";
-  const db = app.database();
-  console.log("T component");
-
-  const getTransactions = (username) => {
-    const ref = db.ref(`${username}/transactions`);
-    ref.on("value", (snapshot) => {
-      const data = snapshot.val();
-      console.log(data);
-      setTransactionsData(data);
-    });
-  };
-
-  const [transactionsData, setTransactionsData] = useState({});
-  getTransactions(username);
+  const tData = [];
 
   return (
     <Main className="Transactions">
       <Navbar />
-      <Section>
+      <div>
         <p>
-          <pre>{transactionsData}</pre>
+          <pre>{JSON.stringify(tData)}</pre>
         </p>
-      </Section>
+      </div>
     </Main>
   );
 };
